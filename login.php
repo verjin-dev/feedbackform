6f1d83a40c3fc991a862fddc3c045288bf736290
Verjin V <verjin2001@gmail.com>
2021-11-27 14:57:02 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+session_start();
+include('./db_connect.php');
+  ob_start();
+  // if(!isset($_SESSION['system'])){
+
+    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
+    foreach($system as $k => $v){
+      $_SESSION['system'][$k] = $v;
+    }
+  // }
+  ob_end_flush();
+?>
+<?php 
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+<?php include 'header.php' ?>
+
+<style>
+ 
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+  
+  
+}
+
+#example1 {
+  border: 2px ;
+  border-radius:50px 20px ;
+}
+
+#example2 {
+  border: 5px ;
+  border-radius:50px 20px ;
+  background: linear-gradient(-95deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	
+}
+
+
+</style>
+
+<body class="hold-transition login-page bg-white">
+  
+  <br>
+  
+  <a href="login.php"><img src="assets\uploads\logo.jpg" id="example1" ></a>
+  
+  <br>
+
+<div class="login-box" >
+
+  <div class="login-logo">
+  
+    <a href="#" class="text-white"></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card" id="example2" >
+    <div class="card-body login-card-body" id="example2" >
+    <center><h2><b>FEEDBACK FORM LOGIN</b></h2></center>
+      <form action="" id="login-form">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" required placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" required placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="form-group mb-3">
+          <label for="">Login As</label>
+          <select name="login" id="" class="custom-select custom-select-sm">
+            <option value="3">Student</option>
+            <option value="2">Faculty</option>
+            <option value="1">Hod/Principal</option>
+          </select>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+ 
+<!-- /.login-box -->
+<script>
+  $(document).ready(function(){
+    $('#login-form').submit(function(e){
+    e.preventDefault()
+    start_load()
+    if($(this).find('.alert-danger').length > 0 )
+      $(this).find('.alert-danger').remove();
+    $.ajax({
+      url:'ajax.php?action=login',
+      method:'POST',
+      data:$(this).serialize(),
+      error:err=>{
+        console.log(err)
+        end_load();
+
+      },
+      success:function(resp){
+        if(resp == 1){
+          location.href ='index.php?page=home';
+        }else{
+          $('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+          end_load();
+        }
+      }
+    })
+  })
+  })
+</script>
+<?php include 'footer.php' ?>
+
+</body>
+</html>
